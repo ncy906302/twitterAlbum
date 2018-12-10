fd97642da971b5189d14cefc898e6a3e5f26dc22
ncy906302 <ncy906302@gmail.com>
2018-12-11 04:08:18 +0900

    ok

--- twitterMedia/wsgi.py
+++ twitterMedia/wsgi.py
@@ -11,8 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from dj_static import Cling
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'twitterMedia.settings')
 
 application = get_wsgi_application()
